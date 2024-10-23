1f95c1b67c7f76167e302161584b34ff1d788217
kanika-07cs <kanikaab.cs24@bitsathy.ac.in>
2024-10-23 22:15:36 +0530

    modified

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const input=document.getElementsByClassName("enter");
-const list=document.getElementsByClassName("list");
+const input=document.getElementById("enter");
+const list=document.getElementById("list");
+
 function addTask(){
-    if(input===''){
+    if(input.value===''){
         alert("Please enter something");
     }
     else{
         let li=document.createElement("li");
         li.innerHTML=input.value;
         list.appendChild(li);
+        let cross=document.createElement("cross");
+        cross.innerHTML="\u00d7";
+        li.appendChild(cross);
     }
+    input.value="";
 }
+list.addEventListener("click",function(on){
+    if(on.target.tagName=="LI"){
+        on.target.classList.toggle("checked");
+    }
+    else if(on.target.tagName=="CROSS"){
+        on.target.parentElement.remove();
+    }
+},false)
